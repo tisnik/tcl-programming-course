8eeb52c132801a9e6e1797a3d053a07c009712e2
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-21 08:20:31 +0200

    Added example: if-then-else.tcl

---
+++ examples/if-then-else.tcl
@@ -0,0 +1,14 @@
+proc porovnej {a b} {
+    puts "zkousim porovnat $a s $b"
+
+    if {$a > $b} then {
+        puts "$a je vetsi nez $b"
+    } else {
+        puts "$a je mensi nez $b"
+    }
+
+    puts "konec"
+}
+
+porovnej 10 2
+porovnej 2 10
