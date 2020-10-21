2eb2058f5202ecc8d068bb2ca9f95eb89ee72a98
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-21 13:13:23 +0200

    Added example: git add assoc_array.tcl

---
+++ examples/assoc_array.tcl
@@ -0,0 +1,10 @@
+set pole(klic1) hodnota1
+set pole(klic2) hodnota2
+
+puts $pole(klic1)
+puts $pole(klic2)
+
+puts [array get pole]
+puts [array names pole]
+puts [array exists pole]
+puts [array exists neexistujici_pole]
