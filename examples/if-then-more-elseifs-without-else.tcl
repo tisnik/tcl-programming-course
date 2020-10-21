c197794d0f76a59528ea7ce7cb2351ea50c02a85
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-21 09:21:27 +0200

    if-elseif without else

---
+++ examples/if-then-more-elseifs-without-else.tcl
@@ -0,0 +1,24 @@
+proc hodnota {a} {
+    puts "zjistuji hodnotu $a"
+
+    if {$a > 1000000} then {
+        puts "$a je vetsi nez milion"
+    } elseif {$a > 1000} {
+        puts "$a je vetsi nez tisic"
+    } elseif {$a > 10} {
+        puts "$a je vetsi nez deset"
+    } elseif {$a > 0} {
+        puts "$a je vetsi nez nula"
+    } {
+        puts "$a je zaporne"
+    }
+
+    puts "konec"
+}
+
+hodnota 1000000000
+hodnota 20000
+hodnota 20
+hodnota 5
+hodnota -5
+
