4b66224f578b9a0d2ca09422613f6eba9d0698ec
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-22 08:14:46 +0200

    Added example: git add foreach1.tcl

---
+++ examples/foreach1.tcl
@@ -0,0 +1,11 @@
+set seznam1 { prvni druhy treti }
+
+foreach prvek $seznam1 {
+    puts $prvek
+}
+
+foreach i {1 2 3} j {4 5 6 7} {
+    puts "i= $i"
+    puts "j= $j"
+    puts "---------------"
+}
