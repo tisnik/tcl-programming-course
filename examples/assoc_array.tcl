f379ab53095f338599429ce1c283ee79b4a38aa5
Pavel Tisnovsky <ptisnovs@redhat.com>
2020-10-22 13:02:29 +0200

    Added comments

--- examples/assoc_array.tcl
+++ examples/assoc_array.tcl
@@ -1,10 +1,16 @@
 set pole(klic1) hodnota1
 set pole(klic2) hodnota2
 
+# pristup k ulozenym hodnotam pod danymi klici
 puts $pole(klic1)
 puts $pole(klic2)
 
+# ziskani klicu a soucasne hodnot
 puts [array get pole]
+
+# ziskani pouze klicu
 puts [array names pole]
+
+# test, zda se jedna o pole
 puts [array exists pole]
 puts [array exists neexistujici_pole]
